db1091711f73110bcec9fb33fddaa795ce4e4e2b
leandro-etec-2025 <leandrocesardacruz.cps.2025@gmail.com>
2025-10-14 20:31:24 -0300

    Iniciado Projeto git

--- app/src/main/java/com/example/myapplication/ATelaEntradaLogica.java
+++ app/src/main/java/com/example/myapplication/ATelaEntradaLogica.java
@@ -4,11 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class ATelaEntradaLogica extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.a_tela_entrada_design);
     }
 }
